b48a990d5b0ed831d0ce1018d82ea85a1c9d553b
Carter Parrett <cparrett94@hotmail.com>
2020-08-11 15:56:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,8 +72,13 @@ class Dog
     dog.save
     dog
   end
-
-
+ 
+  def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT FROM dogs WHERE id = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql)
+  end
 
 
 
