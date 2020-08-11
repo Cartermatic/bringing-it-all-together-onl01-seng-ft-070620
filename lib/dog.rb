32896a93f5fbc4647065d2d3c0ec526d085ab55d
Carter Parrett <cparrett94@hotmail.com>
2020-08-11 15:57:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,9 @@ class Dog
     sql = <<-SQL
     SELECT FROM dogs WHERE id = ? LIMIT 1
     SQL
-    DB[:conn].execute(sql)
+   DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 
