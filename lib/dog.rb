23d3d3fe4d9a6acdd40d3e4cc033c046f86ea8ce
Carter Parrett <cparrett94@hotmail.com>
2020-08-11 14:59:41 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dogs
+  attr_accessor :id, :name, :breed
+  
+  def initialize(id=nil, name, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
